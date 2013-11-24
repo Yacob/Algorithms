277f14c440b97e169fbf1ec79c4a80405c1d2fa0
yacob <nestor.jac@gmail.com>
2013-11-24 16:09:16 -0500

    Start project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//MAIN FUNCTION
+#include <vector>
+#include <iostream>
+#include "string.h"
+#include "string"
+
+
+int main(int argc, char argv*[]){
+	//argv[1] is algorithm to use
+	
+	vector< vector<unsigned int> > 
+	unsigned int num_antennas;
+	unsigned int num_receivers;
+	cin << num_atennas << receivers;
+
+	
+
+
+
+
+}
